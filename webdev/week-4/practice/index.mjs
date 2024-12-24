3deb4ff94378f7c8528b8e08b8a7ccaa8e2d7ea8
Shahbaz-1696 <ansarishahbaz96@gmail.com>
2024-12-24 21:36:14 +0530

    Completed week-4 part-1

---
+++ webdev/week-4/practice/index.mjs
@@ -0,0 +1,7 @@
+import chalk from "chalk";
+// let fullName = "Shahbaz Ahmed Ansari";
+// console.log(fullName);
+
+console.log(chalk.blue("Hello World"));
+console.log(chalk.red.bold("This is an error message"));
+console.log(chalk.green.underline("This is a success message"));
