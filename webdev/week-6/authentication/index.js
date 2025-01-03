12b7f43ef542dee9f94b6216d7b1303aa559b69b
Shahbaz-1696 <ansarishahbaz96@gmail.com>
2025-01-04 00:05:03 +0530

    Completed week-6 auth with jwt

--- webdev/week-6/authentication/index.js
+++ webdev/week-6/authentication/index.js
@@ -1,20 +1,21 @@
 const express = require("express");
 const app = express();
+const jwt = require('jsonwebtoken');
 
 app.use(express.json());
-
+const JWT_SECRET = "iamcrazxy";
 const users = [];
 
-function generateToken() {
-    let token = "";
-    let options = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+// function generateToken() {
+//     let token = "";
+//     let options = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 
-    for (let i = 0; i < 32; i++) {
-        token += options[Math.floor(Math.random() * options.length)];
-    }
+//     for (let i = 0; i < 32; i++) {
+//         token += options[Math.floor(Math.random() * options.length)];
+//     }
 
-    return token;
-}
+//     return token;
+// }
 
 app.post("/signup", function (req, res) {
     const username = req.body.username;
@@ -43,10 +44,12 @@ app.post("/signin", function (req, res) {
     const user = users.find(user => user.username === username && user.password === password);
 
     if (user) {
-        const token = generateToken();
-        user.token = token;
+        const token = jwt.sign({
+            username: username,
+        }, JWT_SECRET); // convert their username to jwt
+        // user.token = token;
         res.json({
-            token
+            token: token,
         });
     } else {
         res.json({
@@ -56,8 +59,11 @@ app.post("/signin", function (req, res) {
 });
 
 app.get("/me", function (req, res) {
-    const token = req.headers.authorization;
-    const user = users.find(user => user.token === token);
+    const token = req.headers.token; // jwt
+
+    const decodedInfo = jwt.verify(token, JWT_SECRET);
+    const username = decodedInfo.username;
+    const user = users.find(user => user.username === username);
 
     if (user) {
         res.json({
