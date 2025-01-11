d815d394db58b5265e898e34db3e7e022f640932
Shahbaz-1696 <ansarishahbaz96@gmail.com>
2025-01-11 14:08:04 +0530

    Completed week-6 assignment-todo

--- week-6/assignments/6.1-todo/backend/routes/todo.js
+++ week-6/assignments/6.1-todo/backend/routes/todo.js
@@ -2,20 +2,79 @@ let todos = []; // in memory space
 
 export async function getAllTodo(req, res, next) {
     //  write here
+    res.json({
+        todos,
+    });
 }
 
 export async function createTodo(req, res, next) {
     //  write here
+    const title = req.body.title;
+
+    const newTodo = todos.push({
+        id: Math.floor(Math.random() * 10000000),
+        title: title,
+    });
+
+    if (!newTodo) {
+        res.status(411).json({
+            message: "Invalid todo",
+        });
+    }
+
+    res.json({
+        id: newTodo.id,
+    });
 }
 
 export async function updateTodo(req, res, next) {
     //  write here
+    const updatedTitle = req.body.title;
+    const id = parseInt(req.params.id);
+    const todoIndex = todos.findIndex(todo => todo.id === id);
+
+    if (todoIndex === -1) {
+        res.status(400).json({
+            message: "Todo not found",
+        });
+    } else {
+        todos[todoIndex].title = updatedTitle;
+        res.json({
+            message: "Todo updated successfully",
+        });
+    }
 }
 
-export async function deleteTodo(req, res, next) {
+export async function searchTodo(req, res, next) {
     //  write here
+    const q = req.query.q;
+
+    if (!q) {
+        return res.status(400).json({
+            message: "Query parameter missing",
+        });
+    }
+
+    const filteredTodos = todos.filter(todo => todo.title.toLowerCase().includes(q.toLowerCase()));
+
+    res.json({
+        filteredTodos,
+    });
 }
 
 export async function deleteTodoById(req, res, next) {
     //  write here
+    const id = parseInt(req.params.id);
+    const todoIndex = todos.findIndex(todo => todo.id === id);
+
+    if (todoIndex === -1) {
+        res.status(411).json({
+            message: "Todo not found",
+        });
+    } else {
+        todos.splice(todoIndex, 1);
+        res.json({
+            message: "Todo deleted successfully",
+        });
+    }
 }
