67373df76a37baa8efbd97429f040c68168bc7ab
Shahbaz-1696 <ansarishahbaz96@gmail.com>
2025-02-06 23:03:05 +0530

    Completed week-13 otp-task

--- week-13/week-13-tailwind/src/Screen1.jsx
+++ week-13/week-13-tailwind/src/Screen1.jsx
@@ -5,8 +5,8 @@ import { Otp } from "./components/Otp.jsx";
 
 const Screen1 = () => {
     return (
-        <div className="flex flex-col justify-center items-center bg-sky-950 h-screen gap-20">
-            <div className="flex gap-5">
+        <div className="flex flex-col justify-center items-center bg-sky-950 min-h-screen gap-20 ">
+            <div className="flex gap-5 mt-32">
                 <img src={PCIcon} alt="pc-icon" width={50} height={20} />
                 <h1 className="font-bold text-4xl"><span className="text-teal-500">Webinar</span>.gg</h1>
             </div>
@@ -14,6 +14,8 @@ const Screen1 = () => {
             <h3 className="text-white font-bold text-2xl">Verify Your Age</h3>
             <p className="text-gray-400 font-semibold ">Please confirm your birth year. This data will not be stored.</p>
             <Input type="date" placeholder="Your Birth Year" />
+            <Button disabled={false}>Continue</Button>
+            <Input type="email" placeholder="Your Email" />
             <Button disabled={false}>Continue</Button>
             <Otp number={6} />
         </div>
--- week-13/week-13-tailwind/src/components/Otp.jsx
+++ week-13/week-13-tailwind/src/components/Otp.jsx
@@ -6,19 +6,20 @@ export const Otp = ({ number }) => {
     const ref = useRef(Array(number).fill(0));
     const [disabled, setDisabled] = useState(false);
     return (
-        <div className="flex">
+        <div className="flex flex-col mb-32">
 
-            {Array(number).fill(1).map((x, index) => <SubOtpBox reference={(e) => ref.current[index] = e} key={index} onDone={() => {
-                if (index + 1 >= number) {
-                    return;
-                }
-                ref.current[index + 1].focus();
-            }} onBack={() => {
-                if (index == 0) {
-                    return;
-                }
-                ref.current[index - 1].focus();
-            }} />)}
+            <div className="flex mb-16">
+                {Array(number).fill(1).map((x, index) => <SubOtpBox reference={(e) => ref.current[index] = e} key={index} onDone={() => {
+                    if (index + 1 < number) {
+                        ref.current[index + 1].focus();
+                    }
+                }} onBack={() => {
+                    if (index > 0) {
+                        ref.current[index - 1].focus();
+                    }
+                }}
+                />)}
+            </div>
 
             <Button disabled={disabled}>Sign Up</Button>
         </div>
@@ -29,15 +30,24 @@ function SubOtpBox({ reference, onDone, onBack }) {
     const [inputBoxVal, setInputBoxVal] = useState("");
     return (
         <input ref={reference} value={inputBoxVal} onKeyUp={(e) => {
-            if (e.key == "Backspace") {
-                onBack();
-                setInputBoxVal("");
+            if (e.key === "Backspace") {
+                if (inputBoxVal == "") {
+                    onBack();
+                } else {
+                    setInputBoxVal("");
+                }
             }
         }} onChange={(e) => {
             const val = e.target.value;
-            if (val == "1" || val == "2" || val == "3" || val == "4" || val == "5" || val == "6" || val == "7" || val == "8" || val == "9" || val == "0") {
+            // if (val == "1" || val == "2" || val == "3" || val == "4" || val == "5" || val == "6" || val == "7" || val == "8" || val == "9" || val == "0") {
+            //     setInputBoxVal(val);
+            //     onDone();
+            // }
+            if (/^\d$/.test(val)) {
                 setInputBoxVal(val);
                 onDone();
+            } else {
+                setInputBoxVal(val === "" ? "" : val.slice(0, 1));
             }
         }} type="text" className="w-[40px] rounded-lg h-[50px] bg-blue-950 mr-4 outline-none px-4 text-white" />
     );
